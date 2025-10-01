51f2bc4ebc583e3b1318581e7470e7f32f2409eb
DoyoSaee <ehdgydi@gmail.com>
2025-10-01 23:55:40 +0900

    add: apollo Server

--- apps/graphQL/src/index.ts
+++ apps/graphQL/src/index.ts
@@ -1,10 +1,12 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-// import { buildSchema } from "graphql";
-
-const app = express();
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { type RequestHandler as ExpressRequestHandler } from "express";
+import http from "http";
 
 const loadedTypes = loadFilesSync("./**/*", {
   extensions: [".graphql"],
@@ -14,25 +16,37 @@ const loadedResolvers = loadFilesSync("./**/*", {
   extensions: [".resolvers.ts", ".resolvers.js"],
 });
 
-const schema = makeExecutableSchema({
-  typeDefs: loadedTypes,
-  resolvers: loadedResolvers,
-});
-
 const port = Number.parseInt(process.env.PORT ?? "5055", 10);
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const schema = makeExecutableSchema({
+    typeDefs: loadedTypes,
+    resolvers: loadedResolvers,
+  });
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-app.listen(port, () => {
+  await server.start();
+
+  const corsMiddleware = cors() as unknown as ExpressRequestHandler;
+  const jsonBodyParser = bodyParser.json() as unknown as ExpressRequestHandler;
+  const apolloMiddleware = expressMiddleware(server) as unknown as ExpressRequestHandler;
+
+  app.use("/graphql", corsMiddleware, jsonBodyParser, apolloMiddleware);
+
+  app.get("/", (_req, res) => {
+    res.send(`GraphQL service running on port ${port}`);
+  });
+
+  await new Promise<void>((resolve) => {
+    httpServer.listen(port, resolve);
+  });
+
   console.log(`GraphQL service running on port ${port}`);
-});
+}
 
-app.get("/", (_req, res) => {
-  res.send("GraphQL service running on port " + port);
-});
+startApolloServer();
